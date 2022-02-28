e6bdd206d3938fff244428dcfde434cd8da5955d
alexismarroquin7 <alexismarroquin754@gmail.com>
2022-02-27 20:16:10 -0800

    axios instace

---
+++ utils/axios-instance.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const axiosInstance = () => axios.create({
+  baseURL: process.NODE_ENV === 'development' ? `http://localhost:9000/api` : `https://expense-tracker-be-a.herokuapp.com/api`
+});
